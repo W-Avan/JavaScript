cfaf1872a89659879d9f0eb32e7c4e6949eba624
W-Avan <528378795@qq.com>
2019-02-24 14:45:45 +0800

    add defaultpage

--- code/http/file_server.js
+++ code/http/file_server.js
@@ -16,17 +16,48 @@ var server = http.createServer(function (request, response){
     var 
         pathname = url.parse(request.url).pathname,   // '/static/bootstrap.css'
         filepath = path.join(root, pathname);   // '/srv/www/static/bootstrap.css'
-    fs.stat(filepath, function (err, stats) {
-        if(!err && stats.isFile()) {
-            console.log('200 ' + request.url);
-            response.writeHead(200);
-            fs.createReadStream(filepath).pipe(response);
-        } else {
-            console.log('404 ' + request.url);
+
+    
+        var defaultPages = ['index.html', 'default.html'];
+    
+        var pageCount = 0;
+        function getDefaultPage() {
+            if(pageCount === defaultPages.length) {
+                get404Page();
+                return;
+            }
+            var page = path.join(filepath, defaultPages[pageCount]);
+            fs.stat(page, function(err, stats) {
+                if(err || !stats.isFile()) {
+                    pageCount++;
+                    getDefaultPage();
+                } else {
+                    get200Page(page);
+                }
+            });
+        }
+    
+        function get404Page() {
+            console.log('404 ' + require.url);
             response.writeHead(404);
             response.end('404 Not Found');
         }
-    });
+    
+        function get200Page(filepath) {
+            console.log('200 ' + request.url);
+            response.writeHead(200);
+            fs.createReadStream(filepath).pipe(response);
+        }
+    
+        fs.stat(filepath, function (err, stats) {
+            if(err){
+                get404Page();
+            }else if(stats.isFile()) {
+                get200Page(filepath);
+            } else if (stats.isDirectory()) {
+                getDefaultPage();
+            }
+        });
 });
 
 server.listen(8080);
